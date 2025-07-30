3aa8c3a5cdb912b9ce026913b60143cf7a79fd49
james <jameswarriner0410@icloud.com>
2025-07-30 17:46:24 +0100

    Update

--- backend/controllers/converter.js
+++ backend/controllers/converter.js
@@ -1,51 +1,51 @@
-const { spawn } = require('child_process');
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-const archiver   = require('archiver');
+
+const { spawn }     = require('child_process');
+const ffmpegPath    = require('@ffmpeg-installer/ffmpeg').path;
+const archiver      = require('archiver');
 
 exports.single = (req, res) => {
   if (!req.file || !req.file.buffer) {
     return res.status(400).json({ error: 'No file uploaded' });
   }
 
-
   const outName = req.file.originalname.replace(/\.mts$/i, '.mp4');
   res.writeHead(200, {
     'Content-Type':        'video/mp4',
-    'Content-Disposition': `attachment; filename="${outName}"`,
-    
+    'Content-Disposition': `attachment; filename="${outName}"`
   });
 
-  
   const args = [
     '-hide_banner',
     '-loglevel', 'error',
-    '-f', 'mpegts',
-    '-i', 'pipe:0',          
-    '-c:v', 'copy',         
-    '-c:a', 'aac',           
-    '-b:a', '320k',         
-    '-bsf:a', 'aac_adtstoasc', 
-    '-movflags', '+faststart', 
-    '-f', 'mp4',
-    'pipe:1'                 
-  ];
-  const ff = spawn(ffmpegPath, args);
 
-  
+
+    '-f', 'mpegts',
+    '-i', 'pipe:0',
+
+
+    '-c:v', 'copy',
+
+
+    '-c:a', 'aac',
+    '-b:a', '320k',
+
+
+    '-bsf:a', 'aac_adtstoasc',
+
+
+    '-f', 'mp4',
+    'pipe:1'
+  ];
+
+  const ff = spawn(ffmpegPath, args);
   ff.stdin.write(req.file.buffer);
   ff.stdin.end();
-
-  
   ff.stdout.pipe(res);
 
   ff.stderr.on('data', d => console.error('FFmpeg stderr:', d.toString()));
   ff.on('error', err => {
     console.error('FFmpeg spawn error:', err);
     if (!res.headersSent) res.status(500).end();
-  });
-  ff.on('close', code => {
-    if (code !== 0) console.error(`FFmpeg exited with code ${code}`);
-
   });
 };
 
@@ -56,15 +56,14 @@ exports.bulk = (req, res) => {
 
   res.writeHead(200, {
     'Content-Type':        'application/zip',
-    'Content-Disposition': 'attachment; filename="converted.zip"',
+    'Content-Disposition': 'attachment; filename="converted.zip"'
   });
 
   const archive = archiver('zip', { zlib: { level: 0 } });
   archive.pipe(res);
 
   req.files.forEach(file => {
-    const inName  = file.originalname;
-    const base    = inName.replace(/\.mts$/i, '');
+    const base    = file.originalname.replace(/\.mts$/i, '');
     const outName = `${base}.mp4`;
 
     const args = [
@@ -76,24 +75,21 @@ exports.bulk = (req, res) => {
       '-c:a', 'aac',
       '-b:a', '320k',
       '-bsf:a', 'aac_adtstoasc',
-      '-movflags', '+faststart',
       '-f', 'mp4',
       'pipe:1'
     ];
-    const ff = spawn(ffmpegPath, args);
 
-    
+    const ff = spawn(ffmpegPath, args);
     ff.stdin.write(file.buffer);
     ff.stdin.end();
 
-    
     archive.append(ff.stdout, { name: outName });
 
     ff.stderr.on('data', d =>
-      console.error(`FFmpeg stderr [${inName}]:`, d.toString())
+      console.error(`FFmpeg stderr [${file.originalname}]:`, d.toString())
     );
     ff.on('error', err =>
-      console.error(`FFmpeg spawn error [${inName}]:`, err)
+      console.error(`FFmpeg spawn error [${file.originalname}]:`, err)
     );
   });
 
