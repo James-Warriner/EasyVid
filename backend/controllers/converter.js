a6eb2537b1eb81fc7ad1697d04ecc92e755f7d7b
james <jameswarriner0410@icloud.com>
2025-07-31 09:25:07 +0100

    Update

--- backend/controllers/converter.js
+++ backend/controllers/converter.js
@@ -4,8 +4,8 @@ const ffmpeg = require('fluent-ffmpeg');
 const stream = require('stream');
 const archiver = require('archiver');
 const tmp = require('tmp');
-const fs = require('fs');
 const path = require('path');
+
 
 
 exports.single = (req, res) => {
@@ -15,34 +15,42 @@ exports.single = (req, res) => {
   const originalName = file.originalname;
   const outputName = originalName.replace(/\.mts$/i, '.mp4');
 
- 
+
   const tmpFile = tmp.fileSync({ postfix: '.mp4' });
   const tmpPath = tmpFile.name;
 
- 
   ffmpeg()
     .input(new stream.PassThrough().end(file.buffer))
     .inputFormat('mpegts')
-    .videoCodec('copy')      
-    .audioCodec('aac')     
-    .audioBitrate('128k')    
+
+    
+    .videoCodec('libx264')
+    .outputOptions([
+      '-preset fast',      
+      '-crf 23',           
+      '-profile:v main',    
+      '-level 3.1',     
+      '-movflags faststart'
+    ])
+
+
+    .audioCodec('aac')
+    .audioBitrate('128k')
+
     .format('mp4')
-    .outputOptions(['-movflags', 'faststart'])
     .output(tmpPath)
+
     .on('start', cmd => console.log('FFmpeg single start:', cmd))
     .on('error', (err, stdout, stderr) => {
       console.error('FFmpeg single error:', err.message);
       console.error(stderr);
-      // Clean up temp
       tmpFile.removeCallback();
       if (!res.headersSent) res.status(500).send('Conversion failed');
     })
     .on('end', () => {
- 
       res.download(tmpPath, outputName, err => {
-
         tmpFile.removeCallback();
-        if (err) console.error('Send error:', err);
+        if (err) console.error('Download error:', err);
       });
     })
     .run();
@@ -55,21 +63,32 @@ exports.bulk = (req, res) => {
 
 
   const tmpDir = tmp.dirSync({ unsafeCleanup: true });
-  const convertTasks = files.map(file => {
-    const inputBuffer = file.buffer;
-    const outputName = file.originalname.replace(/\.mts$/i, '.mp4');
-    const tmpPath = path.join(tmpDir.name, outputName);
 
+
+  const tasks = files.map(file => {
     return new Promise((resolve, reject) => {
+      const outputName = file.originalname.replace(/\.mts$/i, '.mp4');
+      const tmpPath = path.join(tmpDir.name, outputName);
+
       ffmpeg()
-        .input(new stream.PassThrough().end(inputBuffer))
+        .input(new stream.PassThrough().end(file.buffer))
         .inputFormat('mpegts')
-        .videoCodec('copy')
+
+        .videoCodec('libx264')
+        .outputOptions([
+          '-preset fast',
+          '-crf 23',
+          '-profile:v main',
+          '-level 3.1',
+          '-movflags faststart'
+        ])
+
         .audioCodec('aac')
         .audioBitrate('128k')
+
         .format('mp4')
-        .outputOptions(['-movflags', 'faststart'])
         .output(tmpPath)
+
         .on('start', cmd => console.log(`FFmpeg bulk start for ${file.originalname}:`, cmd))
         .on('error', (err, stdout, stderr) => {
           console.error(`FFmpeg bulk error for ${file.originalname}:`, err.message);
@@ -81,9 +100,9 @@ exports.bulk = (req, res) => {
     });
   });
 
-  Promise.all(convertTasks)
+
+  Promise.all(tasks)
     .then(() => {
- 
       res.setHeader('Content-Type', 'application/zip');
       res.setHeader('Content-Disposition', 'attachment; filename="converted_videos.zip"');
 
@@ -91,21 +110,21 @@ exports.bulk = (req, res) => {
       archive.on('error', err => {
         console.error('Archive error:', err.message);
         res.status(500).send('Archive creation failed');
+        tmpDir.removeCallback();
       });
 
       archive.pipe(res);
       files.forEach(file => {
         const name = file.originalname.replace(/\.mts$/i, '.mp4');
-        const filePath = path.join(tmpDir.name, name);
-        archive.file(filePath, { name });
+        archive.file(path.join(tmpDir.name, name), { name });
       });
       archive.finalize();
 
-     
       res.on('finish', () => tmpDir.removeCallback());
     })
     .catch(err => {
-
+      console.error('Bulk conversion error:', err);
       tmpDir.removeCallback();
+      if (!res.headersSent) res.status(500).send('Conversion failed');
     });
 };
